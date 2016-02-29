37aa7670387dd62e58b32ce9d8468b4a3b815768
David Sheldrick <david@futurice.com>
2016-02-29 12:12:36 +0000

    build config fix

--- src/build.js
+++ src/build.js
@@ -1,5 +1,10 @@
+const metalsmith = require('./metalsmith')
+const path = require('path');
+const DEFAULT_CONFIG = require('./config');
+
 function build(config, cb) {
-  const smith = metalsmith(config, 'build')
+  config = Object.assign({}, DEFAULT_CONFIG, config);
+  metalsmith(config, 'build')
     .destination(path.join(config.buildPath, "master"))
     .build(cb);
 }
